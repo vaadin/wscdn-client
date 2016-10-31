6a5e2fa9b1c64227f16a3afa6c786671bd6e7f7f
Henri Sara <hesara@vaadin.com>
2016-10-31 17:05:09 +0200

    Remove Java 7 import

--- src/test/java/com/vaadin/wscdn/client/test/TestClient.java
+++ src/test/java/com/vaadin/wscdn/client/test/TestClient.java
@@ -11,7 +11,6 @@ import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
